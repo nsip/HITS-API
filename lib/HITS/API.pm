b496a22879b1c519edf2691b2d7d3b52b0280c96
Scott Penrose <scottp@dd.com.au>
2014-04-01 23:03:44 +1100

    Initial API docs

--- lib/HITS/API.pm
+++ lib/HITS/API.pm
@@ -1,10 +1,24 @@
 package HITS::API;
+use perl5i::2;
+
 use Dancer ':syntax';
+use Dancer::Plugin::REST;
+use Dancer::Plugin::Database;
+
+use HITS::API:::
 
 our $VERSION = '0.1';
+prefix undef;
+set serializer => 'JSON';
 
 get '/' => sub {
-    template 'index';
+	return {
+		xxx => {
+			title => "xxx",
+			description => "List of xxx and access to some data",
+			href => "" . uri_for('xxx/'),
+		},
+	}
 };
 
 true;
---
+++ lib/HITS/API/Tags.pm
@@ -0,0 +1,32 @@
+package HITS::API::Tags;
+use perl5i::2;
+use Dancer ':syntax';
+use Dancer::Plugin::REST;
+use Dancer::Plugin::Database;
+
+our $VERSION = '0.1';
+prefix '/tags';
+set serializer => 'JSON';
+
+# PURPOSE: Basic tags
+
+# Get current list of tags
+get '/' => sub {
+	my $base = uri_for('/action/'). "";
+	my $sth = database->prepare(qq{
+		SELECT
+			*
+		FROM
+			action
+	});
+	$sth->execute();
+	return {
+		action => $sth->fetchall_arrayref({}),
+	};
+};
+
+get '/:id' => sub {
+};
+
+true;
+
