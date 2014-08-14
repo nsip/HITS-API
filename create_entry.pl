4bfbf7aacc79a8366486ec28f4fe264346dc65a0
Scott Penrose <scottp@dd.com.au>
2014-08-14 04:48:10 +0000

    Output sensible values

--- create_entry.pl
+++ create_entry.pl
@@ -67,7 +67,7 @@ if ($sis->{sis_type} ne 'hits_database') {
 my $dbh_sis;
 {
 	my $dsn = $config->{mysql_dsn_template};
-	my $db = $sis->{sif_ref};
+	my $db = $sis->{sis_ref};
 	$dsn =~ s/TEMPLATE/$db/;
 	$dbh_sis = DBI->connect(
 		$dsn,
@@ -90,21 +90,27 @@ my $user_token = uuid();	# UUID for User token, also equals zone.
 # MAKE - sif.SIF3_APP_TEMPLATE entry - generate Token & Password
 my $sif_template;
 {
-	my $sth = $dbh_sif->prepare(q{
-		INSERT INTO SIF3_APP_TEMPLATE
-			(SOLUTION_ID, APPLICATION_KEY, PASSWORD, USER_TOKEN, AUTH_METHOD)
-		VALUES
-			('HITS', ?, ?, ?, 'Basic')
-	});
-	$sth->execute($app_key, $password, $user_token);
+	my $sth = $dbh_sif->prepare(q{SELECT max(APP_TEMPLATE_ID) as max FROM SIF3_APP_TEMPLATE});
+	$sth->execute();
+	my $max = $sth->fetchrow_hashref;
+	my $id = $max->{max} + 1;
+	print "GENERATING APP TEMPLATE - $app_key, $password, $user_token = $id\n";
 	$sth = $dbh_sif->prepare(q{
-		SELECT APP_TEMPLATE_ID FROM SIF3_APP_TEMPLATE WHERE APPLICATION_KEY = ?
+		INSERT INTO SIF3_APP_TEMPLATE
+			(APP_TEMPLATE_ID, SOLUTION_ID, APPLICATION_KEY, PASSWORD, USER_TOKEN, AUTH_METHOD)
+		VALUES
+			(?, 'HITS', ?, ?, ?, 'Basic')
+	});
+	$sth->execute($id, $app_key, $password, $user_token);
+	$sth = $dbh_sif->prepare(q{
+		SELECT * FROM SIF3_APP_TEMPLATE WHERE APPLICATION_KEY = ?
 	});
 	$sth->execute($app_key);
 	$sif_template = $sth->fetchrow_hashref;
 }
 
 # MAKE - sif.Zone using (Token?)
+my $zone;
 {
 	my $sth = $dbh_sif->prepare(q{
 		INSERT INTO Zone
@@ -113,17 +119,20 @@ my $sif_template;
 			(?, ?)
 	});
 	$sth->execute($user_token, $sis->{sis_ref});	 # TODO Check sis_ref is same format
+	$sth = $dbh_sif->prepare(q{SELECT * FROM Zone WHERE zoneId = ?});
+	$sth->execute($user_token);
+	$zone = $sth->fetchrow_hashref();
 }
 
 # MAKE - sif.Zone_School - map all schools from the SIS
 {
-	my $sth = $dbh_sif->prepare('SELECT RefId FROM SchoolInfo');
+	my $sth = $dbh_sis->prepare('SELECT RefId FROM SchoolInfo');
 	$sth->execute();
 	my $ins = $dbh_sif->prepare(q{
 		INSERT IGNORE INTO Zone_School (zone_id, SchoolInfo_RefId) VALUES (?, ?)
 	});
 	while (my $ref = $sth->fetchrow_hashref) {
-		$ins->execute($user_token, $ref->{RefId});
+		$ins->execute($zone->{id}, $ref->{RefId});
 	}
 }
 
@@ -138,6 +147,15 @@ my $sif_template;
 	$sth->execute($app_id, $sif_template->{APP_TEMPLATE_ID});
 }
 
+print "\n\nCREATE\n";
+print " Vendor = " . $app->{vendor_id} . "\n";
+print " APP ID = " . $app->{id} . "\n";
+print " Database = " . $sis->{sis_ref} . "\n";
+print " Solution ID = " . $sif_template->{SOLUTION_ID} . "\n";
+print " APP Key = " . $sif_template->{APPLICATION_KEY} . "\n";
+print " Password = " . $sif_template->{PASSWORD} . "\n";
+print " User Token = " . $sif_template->{USER_TOKEN} . "\n";
+print " Access to all schools in DB\n";
 
 exit;
 
